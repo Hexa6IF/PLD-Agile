6986d1ece1e0d2643465eca240378816dc90e9ef
Gibs13 <gsgwlol@gmail.com>
2019-11-22 11:53:55 +0100

    branche test

---
+++ Deliferoo/src/h4133/deliferoo/controller/ControllerTest.java
@@ -0,0 +1,17 @@
+package h4133.deliferoo.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControllerTest {
+
+	Controller testController = new Controller();
+	
+	@Test
+	public void test() {
+		
+		fail("Not yet implemented");
+	}
+
+}
