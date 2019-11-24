bc7830e2cc0c4f9f0e10be97e0411b954888452a
teckwan <wong.teck.wan@outlook.com>
2019-11-24 23:29:39 +0100

    Add menu bar with file loading

--- src/main/deliferoo/view/Window.java
+++ src/main/deliferoo/view/Window.java
@@ -1,6 +1,15 @@
 package view;
 
+import java.io.File;
+
 import controller.Controller;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Window {
@@ -13,15 +22,58 @@ public class Window {
     
     public void launchWindow() {
 	Stage stage = new Stage();
-	buildAndShowScene(stage);	
+	buildAndShowStage(stage);	
     }
     
-    private void buildAndShowScene(Stage stage) {
+    private void buildAndShowStage(Stage stage) {
 	stage.setTitle("Del'IFeroo");
 	
-	stage.setFullScreen(true);
 	stage.setResizable(false);
+	
+	stage.setScene(getScene(stage));
 	
 	stage.show();
     }
+    
+    private Scene getScene(Stage stage) {
+	BorderPane border = new BorderPane();
+	
+	VBox menu = createMenu(stage);
+	
+	border.setTop(menu);
+	
+	return new Scene(border);
+    }
+    
+    private VBox createMenu(Stage stage) {
+	FileChooser fileChooser = new FileChooser();
+	
+	Menu menuFile = new Menu("File");
+	Menu menuHelp = new Menu("Help");
+	
+	MenuItem loadMap = new MenuItem("Load Map...");
+	MenuItem loadDeliveries = new MenuItem("Load Deliveries...");
+	
+	MenuItem about = new MenuItem("About");
+	
+	MenuBar menuBar = new MenuBar();
+	
+	menuFile.getItems().add(loadMap);
+	menuFile.getItems().add(loadDeliveries);
+	
+	menuHelp.getItems().add(about);
+	
+	loadMap.setOnAction(e -> {
+	    File mapFile = fileChooser.showOpenDialog(stage);
+	});
+	loadDeliveries.setOnAction(e -> {
+	    File deliveriesFile = fileChooser.showOpenDialog(stage);
+	});
+	
+	menuBar.getMenus().addAll(menuFile, menuHelp);
+	
+	return new VBox(menuBar);
+    }
+    
+    
 }
