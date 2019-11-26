b865b6a8ee43464603bf8fef7f6dfbc3b62cd544
hakimaab <55960458+hakimaab@users.noreply.github.com>
2019-11-26 20:36:00 +0100

    testing parse xml file for a city plan

--- src/test/deliferoo/xml/XMLParserTest.java
+++ src/test/deliferoo/xml/XMLParserTest.java
@@ -1,13 +1,29 @@
 package xml;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
 public class XMLParserTest {
 
     @Test
     public void test() {
-	fail("Not yet implemented");
+
+	  try {
+	      File fXmlFile = new File("C:\\Users\\PC\\Downloads\\fichiersXML2019\\fichiersXML2019\\petitPlan.xml");
+	      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+             Document doc = dBuilder.parse(fXmlFile);
+             XMLParser.parserxml(doc);
+             assertEquals(616,XMLParser.get1());
+             assertEquals(308,XMLParser.get2());
+                }
+	  catch(Exception e) {}
     }
 
 }
