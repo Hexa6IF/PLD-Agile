5b2d23010917e6a1cfdfcfe0df33db6400acd52b
Arteox <ung.louis@hotmail.fr>
2019-11-26 17:54:57 +0100

    added tests

--- src/test/deliferoo/model/NodeTest.java
+++ src/test/deliferoo/model/NodeTest.java
@@ -10,7 +10,7 @@ public class NodeTest {
 	Node node1 = new Node((long)25175791, (float)45.75406, (float)4.857418);
 	Node node2 = new Node((long)2129259178, (float)45.750404, (float)4.8744674);
 	Float distance = node1.calculateDistance(node2);
-	assertEquals(distance, (float)1.39);
+	assertEquals(distance, (float)1.3836797);
     }
 
 }
