5d6b6afd6230bec2d0232559fcbe2e624773b307
sadsithalokuge <slokuge97@gmail.com>
2019-11-29 10:55:45 +0100

    Added markers

--- src/main/deliferoo/view/MapView.java
+++ src/main/deliferoo/view/MapView.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.util.List;
+import java.util.Random;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Tooltip;
@@ -15,8 +16,10 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import model.BestPath;
+import model.Delivery;
 import model.Edge;
 import model.FullMap;
+import model.SpecialNode;
 
 /**
  * Class for displaying full map
@@ -39,7 +42,7 @@ public class MapView {
      * 
      * @param nodeList List of nodes to display on graph
      */
-    public MapView(FullMap map, Double screenHeight, Double screenWidth ) {
+    public MapView(FullMap map, Double screenHeight, Double screenWidth) {
 	this.mapView = new Pane();
 	this.map = map;
 
@@ -52,55 +55,62 @@ public class MapView {
 
     public Pane getMapView() {
 	draw();
-	this.mapView.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(5), 
+	this.mapView.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(5),
 		BorderWidths.DEFAULT, new Insets(10))));
 	return this.mapView;
     }
 
     private void draw() {
 	Double dimension = Math.min(this.width - 2 * this.offsetX, this.height - 4 * this.offsetY);
-	for(Edge edge : map.getEdgeList()) {
+	for (Edge edge : map.getEdgeList()) {
 	    this.mapView.getChildren().add(createRoad(edge, dimension));
 	}
     }
 
     private Line createRoad(Edge edge, Double dimension) {
-	Double x1 = this.offsetY + dimension * ((edge.getStart().getLongitude() - this.map.getMinLong()) / 
-		this.map.getRangeLongitude());
-	Double y1 = this.offsetX + dimension * ((edge.getStart().getLatitude() - this.map.getMinLat()) / 
-		this.map.getRangeLatitude());
-	
-	Double x2 = this.offsetY + dimension * ((edge.getEnd().getLongitude() - this.map.getMinLong()) / 
-		this.map.getRangeLongitude());
-	Double y2 = this.offsetX + dimension * ((edge.getEnd().getLatitude() - this.map.getMinLat()) / 
-		this.map.getRangeLatitude());
-	
+	Double x1 = this.offsetY
+		+ dimension * ((edge.getStart().getLongitude() - this.map.getMinLong()) / this.map.getRangeLongitude());
+	Double y1 = this.offsetX
+		+ dimension * ((edge.getStart().getLatitude() - this.map.getMinLat()) / this.map.getRangeLatitude());
+
+	Double x2 = this.offsetY
+		+ dimension * ((edge.getEnd().getLongitude() - this.map.getMinLong()) / this.map.getRangeLongitude());
+	Double y2 = this.offsetX
+		+ dimension * ((edge.getEnd().getLatitude() - this.map.getMinLat()) / this.map.getRangeLatitude());
+
 	Line road = new Line(x1, y1, x2, y2);
 	road.setStrokeWidth(2);
 	Tooltip.install(road, new Tooltip(edge.getStreetName()));
-	
+
 	return road;
     }
-    
-    private void drawMarkers(List <BestPath> bestPaths, List<Delivery> deliveries){
+
+    private void drawMarkers(List<BestPath> bestPaths, List<Delivery> deliveries) {
 	Double dimension = Math.min(this.width - 2 * this.offsetX, this.height - 4 * this.offsetY);
-	for(BestPath bestPath : bestPaths) {
-	    this.mapView.getChildren().add(createMarker(bestPath, dimension));
+
+	for (Delivery delivery : deliveries) {
+	    Random rand = new Random();
+	    Double r = rand.nextDouble();
+	    Double g = rand.nextDouble();
+	    Double b = rand.nextDouble();
+	    Color deliveryColor = Color.color(r, g, b);
+	    SpecialNode start = delivery.getPickupNode();
+	    SpecialNode end = delivery.getDeliveryNode();
+	    Circle startMarker = this.createMarker(start, dimension, deliveryColor);
+	    Circle endMarker = this.createMarker(end, dimension, deliveryColor);
+	    this.mapView.getChildren().addAll(startMarker,endMarker);
 	}
-	
-	for(Delivery delivery : deliveries ) {
     }
-    
-    private Circle createMarker(BestPath bestPath, Double dimension, Paint paint) {
-	Double x = this.offsetX + dimension * ((bestPath.getStart().getNode().getLongitude() - this.map.getMinLong()) / 
-		this.map.getRangeLongitude());
-	Double y = this.offsetY + dimension * (( this.map.getMaxLat() - bestPath.getStart().getNode().getLatitude()) / 
-		this.map.getRangeLatitude());
+
+    private Circle createMarker(SpecialNode node, Double dimension, Paint paint) {
+	Double x = this.offsetX + dimension * ((node.getNode().getLongitude() - this.map.getMinLong())
+		/ this.map.getRangeLongitude());
+	Double y = this.offsetY + dimension
+		* ((this.map.getMaxLat() - node.getNode().getLatitude()) / this.map.getRangeLatitude());
 	Circle marker = new Circle(x, y, 5.0, paint);
-	String textToDisplay = bestPath.getStart().toString() + " - " + bestPath.getEnd().toString() ;
+	String textToDisplay = "";
 	Tooltip.install(marker, new Tooltip(textToDisplay));
 	return marker;
     }
-    
-    
+
 }
