2e1573c5552d540d9914dccd185c1281e2bf5d50
teckwan <wong.teck.wan@outlook.com>
2019-11-30 00:57:42 +0100

    Refactor mapView

--- src/main/deliferoo/view/MapView.java
+++ src/main/deliferoo/view/MapView.java
@@ -1,12 +1,9 @@
 package view;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
 
 import javafx.geometry.Insets;
-import javafx.scene.control.Tooltip;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
@@ -14,16 +11,17 @@ import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polyline;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import javafx.util.Pair;
 import model.BestPath;
 import model.Delivery;
 import model.Edge;
 import model.FullMap;
+import model.Node;
 import model.SpecialNode;
 import model.SpecialNodeType;
 
@@ -42,6 +40,7 @@ public class MapView {
 
     private Double offsetX;
     private Double offsetY;
+    private Double dimension;
 
     /**
      * Constructor
@@ -50,6 +49,9 @@ public class MapView {
      */
     public MapView(FullMap map, Double screenHeight, Double screenWidth) {
 	this.mapView = new Pane();
+	this.mapView.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(5),
+		BorderWidths.DEFAULT, new Insets(10))));
+	
 	this.map = map;
 
 	this.height = screenHeight;
@@ -57,122 +59,73 @@ public class MapView {
 
 	this.offsetX = 0.05 * this.width;
 	this.offsetY = 0.05 * this.height;
+	this.dimension = Math.min(this.width - 2 * this.offsetX, this.height - 4 * this.offsetY);
     }
 
     public Pane getMapView() {
-	this.mapView = new Pane();
-	draw(map.getEdgeList(), Color.BLACK, 2);
-
-//	Random rand = new Random();
-//
-//	for (String s : bestPaths.keySet()) {
-//	    System.out.println(s);
-//	}
-//
-//	// 342873658
-//	// 208769039
-//	// 25173820
-//	Double r = rand.nextDouble();
-//	Double g = rand.nextDouble();
-//	Double b = rand.nextDouble();
-//	Color color = Color.color(r, g, b);
-//	draw(bestPaths.get("34401989").getPath(), color, 5);
-
-	this.mapView.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(5),
-		BorderWidths.DEFAULT, new Insets(10))));
 	return this.mapView;
     }
+    
+    public void drawPath(Edge edge, Color color, Integer strokeWidth) {
+	Pair<Double, Double> p1 = calculateRelativePosition(edge.getStart());
+	Pair<Double, Double> p2 = calculateRelativePosition(edge.getEnd());
+	
+	Line path = new Line(p1.getKey(), p1.getValue(), p2.getKey(), p2.getValue());
+	
+	path.setStroke(color);
+	path.setStrokeWidth(strokeWidth);
+	
+	this.mapView.getChildren().add(path);
+    }
+	
+    public void drawMarker(Delivery delivery, Color color, Integer markerSize) {
+	SpecialNode start = delivery.getPickupNode();
+	SpecialNode end = delivery.getDeliveryNode();
 
-    private void draw(List<Edge> edges, Color color, Integer width) {
-	Double dimension = Math.min(this.width - 2 * this.offsetX, this.height - 4 * this.offsetY);
-	for (Edge edge : edges) {
-	    this.mapView.getChildren().add(createRoad(edge, dimension, color, width));
+	for(SpecialNode node : Arrays.asList(start, end)) {
+	    Shape marker = null;
+	    
+	    Pair<Double, Double> p = calculateRelativePosition(node.getNode());
+	    Double x = p.getKey();
+	    Double y = p.getValue();
+	    
+	    if (node.getSpecialNodeType() == SpecialNodeType.PICKUP) {
+		marker = new Circle(x, y, markerSize);
+	    } else if (node.getSpecialNodeType() == SpecialNodeType.DROPOFF) {
+		marker = new Rectangle(x - markerSize/2, y - markerSize/2, markerSize, markerSize);
+	    } else {
+		Polyline triangle = new Polyline();
+		triangle.getPoints().addAll(50d, 0d, 0d, 50d, 100d, 50d, 50d, 0d);
+		marker = triangle;
+	    }
+	    marker.setFill(color);
+	    this.mapView.getChildren().add(marker);
 	}
     }
-
-    private Line createRoad(Edge edge, Double dimension, Color color, Integer width) {
-	Double x1 = this.offsetX
-		+ dimension * (edge.getStart().getLongitude() - this.map.getMinLong()) / this.map.getRangeLongitude();
-	Double y1 = this.offsetY
-		+ dimension * (this.map.getMaxLat() - edge.getStart().getLatitude()) / this.map.getRangeLatitude();
-
-	Double x2 = this.offsetX
-		+ dimension * (edge.getEnd().getLongitude() - this.map.getMinLong()) / this.map.getRangeLongitude();
-	Double y2 = this.offsetY
-		+ dimension * (this.map.getMaxLat() - edge.getEnd().getLatitude()) / this.map.getRangeLatitude();
-
-	Line road = new Line(x1, y1, x2, y2);
-	road.setStroke(color);
-	road.setStrokeWidth(width);
-	Tooltip.install(road, new Tooltip(edge.getStreetName() + " " + edge.getStart().getNodeId() + " | "
-		+ edge.getEnd().getNodeId() + " | " + edge.getDistance()));
-
-	return road;
-    }
-
-    public void drawMarkers(List<Delivery> deliveries) {
-	Double dimension = Math.min(this.width - 2 * this.offsetX, this.height - 4 * this.offsetY);
-
-	for (Delivery delivery : deliveries) {
-	    Random rand = new Random();
-	    Double r = rand.nextDouble();
-	    Double g = rand.nextDouble();
-	    Double b = rand.nextDouble();
-	    Color deliveryColor = Color.color(r, g, b);
-	    SpecialNode start = delivery.getPickupNode();
-	    SpecialNode end = delivery.getDeliveryNode();
-	    Shape startMarker = this.createMarker(start, dimension, deliveryColor);
-	    Shape endMarker = this.createMarker(end, dimension, deliveryColor);
-	    this.mapView.getChildren().addAll(startMarker, endMarker);
-	}
-    }
-
-    private Shape createMarker(SpecialNode node, Double dimension, Paint paint) {
-	Shape marker = null;
-
-	Double x = this.offsetX
-		+ dimension * ((node.getNode().getLongitude() - this.map.getMinLong()) / this.map.getRangeLongitude());
-	Double y = this.offsetY
-		+ dimension * ((this.map.getMaxLat() - node.getNode().getLatitude()) / this.map.getRangeLatitude());
-
-	String textToDisplay = node.getSpecialNodeType() + " : " + node.getDuration();
-
-	if (node.getSpecialNodeType() == SpecialNodeType.PICKUP) {
-	    marker = new Circle(x, y, 10.0, paint);
-	} else if (node.getSpecialNodeType() == SpecialNodeType.DROPOFF) {
-	    marker = new Rectangle(x - 10, y - 10, 20, 20);
-	    marker.setFill(paint);
-	} else {
-	    marker = new Circle(x, y, 10, paint);
-	}
-
-	Tooltip.install(marker, new Tooltip(textToDisplay));
-	return marker;
-    }
-
-    public void drawRound(List<BestPath> bestPaths) {
+    
+    public void drawRound(List<BestPath> bestPaths, Color color, Integer strokeWidth) {
 	Polyline round = new Polyline();
-	round.setStroke(Color.HOTPINK);
-	round.setStrokeWidth(3);
-	Double dimension = Math.min(this.width - 2 * this.offsetX, this.height - 4 * this.offsetY);
+	
+	round.setStroke(color);
+	round.setStrokeWidth(strokeWidth);
+	
 	for (BestPath bestPath : bestPaths) {
 	    List<Edge> path = bestPath.getPath();
 	    for (Edge edge : path) {
-
-		Double x1 = this.offsetX + dimension * (edge.getStart().getLongitude() - this.map.getMinLong())
-			/ this.map.getRangeLongitude();
-		Double y1 = this.offsetY + dimension * (this.map.getMaxLat() - edge.getStart().getLatitude())
-			/ this.map.getRangeLatitude();
-
-		Double x2 = this.offsetX + dimension * (edge.getEnd().getLongitude() - this.map.getMinLong())
-			/ this.map.getRangeLongitude();
-		Double y2 = this.offsetY + dimension * (this.map.getMaxLat() - edge.getEnd().getLatitude())
-			/ this.map.getRangeLatitude();
-		round.getPoints().addAll(new Double[] { x1, y1, x2, y2 });
+		Pair<Double, Double> p1 = calculateRelativePosition(edge.getStart());
+		Pair<Double, Double> p2 = calculateRelativePosition(edge.getEnd());
+		
+		round.getPoints().addAll(p1.getKey(), p1.getValue(), p2.getKey(), p2.getValue());
 	    }
 	}
 
 	this.mapView.getChildren().add(round);
     }
-
+    
+    private Pair<Double, Double> calculateRelativePosition(Node point) {
+	Double x = this.offsetX + dimension * (point.getLongitude() - this.map.getMinLong()) / this.map.getRangeLongitude();
+	Double y = this.offsetY + dimension * (this.map.getMaxLat() - point.getLatitude()) / this.map.getRangeLatitude();
+	
+	return new Pair<Double, Double>(x, y);
+    }
 }
