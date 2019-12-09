d27aedfbe59620623cf01bc0ecf5ccab959954a2
Arteox <ung.louis@hotmail.fr>
2019-12-09 18:13:12 +0100

    1min20

--- src/main/deliferoo/algorithm/TSPHeuristic.java
+++ src/main/deliferoo/algorithm/TSPHeuristic.java
@@ -10,9 +10,6 @@ public class TSPHeuristic extends TSP {
     @Override
     int bound(SpecialNode currentNode, String finishNodeID, ArrayList<SpecialNode> undiscovered, Map<String, Map<String, Integer>> cost) {
 	Integer bound = 0;
-	if (undiscovered.size() == 0) {
-	    return bound;
-	}
 	Integer lengthToCurrent = Integer.MAX_VALUE;
 	String currentNodeID = currentNode.getNode().getNodeId();
 	for (int i=0; i<undiscovered.size(); i++) {
