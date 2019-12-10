e44169300346de6a370e307d879c95b69b74fba8
Gibs13 <gsgwlol@gmail.com>
2019-12-10 08:46:37 +0100

    basic tsp tests

---
+++ src/test/deliferoo/algorithm/TSPSimpleTest.java
@@ -0,0 +1,44 @@
+package algorithm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TSPSimpleTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void testGetTimeLimitReached() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSearchSolutionIntIntIntArrayArrayIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSearchSolutionIntMapOfStringMapOfStringBestPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBestSolution() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBestSolutionCost() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBestPathSolution() {
+		fail("Not yet implemented");
+	}
+
+}
