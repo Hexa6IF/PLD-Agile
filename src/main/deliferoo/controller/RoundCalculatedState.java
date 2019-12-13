0e507c89980309bbb12d6aff35d119065daec4f7
Marie Grenier <marie.grenier@insa-lyon.fr>
2019-12-13 10:03:44 +0100

    Error_State and pikachu and minor corrections

--- src/main/deliferoo/controller/RoundCalculatedState.java
+++ src/main/deliferoo/controller/RoundCalculatedState.java
@@ -46,39 +46,60 @@ public class RoundCalculatedState implements State {
     
     @Override
     public void loadMap(Window window, Controller controller, File mapFile) {
-	FullMap map = XMLParser.getInstance().parseMap(mapFile);
-	if (map.getEdgeList().size() > 0 && map.getNodeMap().size() > 0) {
-	    try {
-		window.updateMap(map);
-		window.updateDeliveries(new ArrayList<Delivery>());
-		controller.setCurrentMap(map);
-		controller.setCurrentState(controller.MAP_LOADED_STATE);
-	    } catch (Exception e) {
-		window.updateMessage("Error in loaded XML file. Please correct it or load another file.");
+	try {
+	    FullMap map = XMLParser.getInstance().parseMap(mapFile);
+	    if (map.getEdgeList().size() > 0 && map.getNodeMap().size() > 0) {
+		try {
+		    window.updateMap(map);
+		    window.updateDeliveries(new ArrayList<Delivery>());
+		    controller.setCurrentMap(map);
+		    controller.setCurrentState(controller.MAP_LOADED_STATE);
+		} catch (Exception e) {
+		    window.updateMessage("Error in loaded XML file. Please correct it or load another file.");
+		    window.clearMap();
+		    controller.setCurrentState(controller.ERROR_STATE);
+		}
+	    } else {
+		window.updateMessage("The loaded XML file does not match the expected format. Please correct it or load another file.");
 		window.clearMap();
+		controller.setCurrentState(controller.ERROR_STATE);
 	    }
-	} else {
-	    window.updateMessage("The loaded XML file does not match the expected format. Please correct it or load another file.");
+	} catch (Exception e) {
+	    window.updateMessage("Syntax error in loaded XML file. Please correct it or load another file.");
+	    window.clearMap();
+	    controller.setCurrentState(controller.ERROR_STATE);
 	}
     }
 
     @Override
     public void loadDeliveries(Window window, Controller controller, File deliveriesFile, FullMap map) {
-	List<Delivery> deliveries = XMLParser.getInstance().parseDeliveries(deliveriesFile, map);
-	if (deliveries.size() > 0) {
-	    try {
-		window.updateDeliveries(deliveries);
-		controller.getCyclist().setDeliveries(deliveries);
-		controller.setCurrentState(controller.CALCULATING_ROUND_STATE);
-	    } catch (Exception e) {
-		window.updateMessage("Error in loaded XML file. Please correct it or load an other file.");
+	try {
+	    List<Delivery> deliveries = XMLParser.getInstance().parseDeliveries(deliveriesFile, map);
+	    if (deliveries.size() > 0) {
+		try {
+		    window.updateDeliveries(deliveries);
+		    controller.getCyclist().setDeliveries(deliveries);
+		    controller.setCurrentState(controller.CALCULATING_ROUND_STATE);
+		} catch (Exception e) {
+		    window.updateMessage("Error in loaded XML file. Please correct it or load an other file.");
+		    window.clearDeliveriesMarkers();
+		    window.updateDeliveries(new ArrayList<>());
+		    window.clearDeliveriesRound();
+		    controller.setCurrentState(controller.MAP_LOADED_STATE);
+		}
+	    } else {
+		window.updateMessage("The loaded XML file does not match the expected format. Please correct it or load an other file.");
 		window.clearDeliveriesMarkers();
-		List<Delivery> emptyDeliveries = new ArrayList<>();
-		window.updateDeliveries(emptyDeliveries);
+		window.updateDeliveries(new ArrayList<>());
 		window.clearDeliveriesRound();
+		controller.setCurrentState(controller.MAP_LOADED_STATE);
 	    }
-	} else {
-	    window.updateMessage("The loaded XML file does not match the expected format. Please correct it or load an other file.");
+	} catch (Exception e) {
+	    window.updateMessage("Syntax error in loaded XML file. Please correct it or load an other file.");
+	    window.clearDeliveriesMarkers();
+	    window.updateDeliveries(new ArrayList<>());
+	    window.clearDeliveriesRound();
+	    controller.setCurrentState(controller.MAP_LOADED_STATE);
 	}
     }
 }
